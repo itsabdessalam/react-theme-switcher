68062b471ade909ca1843515e1f7306f2a2f90fe
Abdessalam98 <belmehelabdessalam.benharira@ynov.com>
2019-05-08 18:49:42 +0200

    add project

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# react-theme-switcher
-Theme switcher react component 
+# React theme switcher component
+
+> A react theme switcher component
+
+## Installation
+
+```bash
+# using yarn
+yarn add react-button-component
+
+# using npm
+npm install react-theme-switcher
+```
+
+## Usage
+
+By default theme switcher is applied to body and colors are already setted
+
+```js
+import React from "react";
+import ThemeSwitcher from "react-theme-switcher";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ThemeSwitcher />
+        <h2>React theme switcher</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+```
+
+Props you can pass to the component:
+
+```js
+<ThemeSwitcher
+    cssSelector="body"
+    darkColor="#282c34"
+    lightColor="#ffffff"
+    buttonColor="#ffffff"
+    switchColor="#2775cc"
+    lightTextColor="#272b33"
+    darkTextColor="#ffffff"
+/>
+```
+
+## Demo
+
+Preview it on CodeSandbox <https://codesandbox.io/s/k0pml1q2v>
+
+![demo](./preview/demo.gif)
+
+## Author
+
+[Abdessalam BENHARIRA](https://github.com/Abdessalam98)
---
+++ src/index.js
@@ -0,0 +1,143 @@
+import React, { Component } from "react";
+import styled, { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    body {
+        transition: background 0.2s cubic-bezier(.75,0,.08,1);
+        &[data-theme='dark'] {
+            color: ${props => props.darkTextColor};
+            background: ${props => props.darkColor};
+        }
+        &[data-theme='light'] {
+            color: ${props => props.lightTextColor};
+            background: ${props => props.lightColor};
+        }
+    }
+`;
+
+const ThemeSwitcherWrapper = styled.div`
+	display: inline-block;
+	width: 45px;
+	height: 20px;
+	text-align: center;
+	position: relative;
+	cursor: pointer;
+
+	label {
+		position: relative;
+		display: inline-block;
+		width: 100%;
+		height: 100%;
+	}
+	input[type="checkbox"] {
+		display: block;
+		width: 100%;
+		height: 100%;
+		opacity: 0;
+		margin: auto;
+		pointer-events: all;
+		cursor: pointer;
+		&:checked ~ .button {
+			left: 25px;
+		}
+	}
+`;
+
+const Button = styled.span`
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 20px;
+	height: 20px;
+	border-radius: 50%;
+	background-color: ${props => props.buttonColor};
+	box-shadow: 0 0.9px 1.8px rgba(0, 0, 0, 0.4);
+	transition: all 0.2s cubic-bezier(0.75, 0, 0.08, 1);
+	pointer-events: none;
+`;
+const Switch = styled.span`
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+	width: 40px;
+	height: 10px;
+	border-radius: 5px;
+	background-color: ${props => props.switchColor};
+	transition: all 0.2s cubic-bezier(0.75, 0, 0.08, 1);
+	pointer-events: none;
+`;
+
+class ThemeSwitcher extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			theme:
+				localStorage.getItem("theme") !== null
+					? localStorage.getItem("theme")
+					: "light"
+		};
+	}
+	componentDidMount = () => {
+		const { theme } = this.state;
+		const { cssSelector } = this.props;
+		const seletor = document.querySelector(cssSelector);
+		seletor.dataset.theme = theme;
+	};
+
+	switchTheme = () => {
+		const { theme } = this.state;
+		const { cssSelector } = this.props;
+		const seletor = document.querySelector(cssSelector);
+		const newTheme = theme === "light" ? "dark" : "light";
+		this.setState({ theme: newTheme });
+		seletor.dataset.theme = theme;
+		localStorage.setItem("theme", this.state.theme);
+	};
+
+	render() {
+		const { theme } = this.state;
+		const {
+			darkColor,
+			lightColor,
+			darkTextColor,
+			lightTextColor,
+			buttonColor,
+			switchColor
+		} = this.props;
+		return (
+			<ThemeSwitcherWrapper>
+				<GlobalStyle
+					darkColor={darkColor}
+					lightColor={lightColor}
+					darkTextColor={darkTextColor}
+					lightTextColor={lightTextColor}
+				/>
+				<label htmlFor="switcher">
+					<input
+						type="checkbox"
+						onChange={this.switchTheme}
+						defaultChecked={theme === "dark"}
+						name="switch"
+						value="switch"
+						aria-describedby="switch"
+					/>
+					<Switch className="switch" switchColor={switchColor} />
+					<Button className="button" buttonColor={buttonColor} />
+				</label>
+			</ThemeSwitcherWrapper>
+		);
+	}
+}
+
+ThemeSwitcher.defaultProps = {
+	cssSelector: "body",
+	darkColor: "#282c34",
+	lightColor: "#ffffff",
+	buttonColor: "#ffffff",
+	switchColor: "#2775cc",
+	lightTextColor: "#272b33",
+	darkTextColor: "#ffffff"
+};
+
+export default ThemeSwitcher;
